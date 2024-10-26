32f606a7cff996268f75c679e5bc64e91602dc40
PointTensor <73913935+PointTensor@users.noreply.github.com>
2024-10-26 18:38:22 +0400

    cleaning

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 
 #include "gof.h"
 
@@ -67,5 +67,27 @@ int main(){
     }
     vec = new_vec;
     }
+
+
+    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+
+
     return 0;
 }
